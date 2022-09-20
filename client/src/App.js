7e0f6ca22d601e38e10810538040bd75369a4ec5
garshakurthi saikumar <saikumarg781@gmail.com>
2022-09-20 15:43:01 +0530

    header created nav bar by saikumar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      
+      <Navbar/>
+    </>
   );
 }
 
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <div className="logo">
+                <p>LAUNDRY</p>
+            </div>
+           <div className="names">
+             <div className="name1">Home</div>
+             <div className="name1">pricing</div>
+             <div className="name1">Career</div>
+             <div className="name2">Sign in</div>
+           </div>
+                 
+        </div>
+
+    );
+}
+
+export default Navbar;
